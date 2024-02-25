b4570339f1266d18e7ea0fa79eca2cd445d0552d
Miguel Contreras <miguelacontreras08@gmail.com>
2024-02-24 19:05:26 -0700

    added mongodb connection to the project

---
+++ pymongo_get_database.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+def get_database():
+
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = "mongodb+srv://miggyco:@MaC42537@whatcanweplay.sttygza.mongodb.net/?retryWrites=true&w=majority&appName=WhatCanWePlay"
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['user_shopping_list']
